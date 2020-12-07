457661e014d0c8b032e8fee7dbdd6f99e88634f8
Kaushik Kumaran <kumarankaushik@gmail.com>
2020-12-07 21:24:28 +0400

    final

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const connect = await mongoose.connect(process.env.MONGODB_URL, {
-      useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
-      useFindAndModify: false
+      useFindAndModify: false,
+      useUnifiedTopology: true,
     });
     console.log(`DataBase Connected : ${connect.connection.host}`);
   } catch (e) {
